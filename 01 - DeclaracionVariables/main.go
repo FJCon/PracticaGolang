82a2f0963379ca2081e8430d1eaf0e186609efdc
FJCon <conilfernando@gmail.com>
2023-04-20 11:42:57 -0300

    Declaracion variables

---
+++ 01 - DeclaracionVariables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+func main()  {
+	var nombre string = "Fernando"
+	var apellido string = "Conil"
+	var edad int = 26
+	fmt.Println(nombre, apellido, edad)
+
+	nombre01 := "Lucia"
+	apellido02 := "Schmidt"
+	edad02 := 27
+	fmt.Println(nombre01, apellido02, edad02)
+}
