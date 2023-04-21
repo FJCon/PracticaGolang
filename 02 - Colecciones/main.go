2144aabe0a0f198f9b008d3d04ca784e3c2205d7
FJCon <conilfernando@gmail.com>
2023-04-21 20:40:08 -0300

    arrays

---
+++ 02 - Colecciones/main.go
@@ -0,0 +1,51 @@
+package main
+import "fmt"
+func main() {
+	//Arrays solo guardan datos del mismo tipo
+	//Se debe indicar la longitud
+
+	var primerArray [5]string
+
+	primerArray[0] = "Fernando"
+	primerArray[1] = "Lucia"
+	primerArray[2] = "Nicolas"
+	primerArray[3] = "Sofia"
+	primerArray[4] = "Ana"
+	
+	//El bucle for no lleva parentesis para iniciar el contador, condicion y post
+	fmt.Println("ARRAY CON BUCLE FOR")
+	fmt.Println("==================")
+	for i := 0 ; i< len(primerArray)-1; i++{
+		fmt.Println(primerArray[i])
+	}
+	fmt.Println("==================")
+
+	//Éste es el equivalente a foreach en Go
+	fmt.Println("ARRAY CON ForEach")
+	fmt.Println("==================")
+	for _, num := range primerArray{
+		fmt.Println(num)
+	}
+	fmt.Println("==================")
+
+	//SLICE = Coleccion de longitud flexible con elementos del mismo tipo.
+	primerSlice := [] int{12,43,210}
+	primerSlice = append(primerSlice, 37) // agrego elemento al slice
+	fmt.Println("SLICE CON ForEach")
+	fmt.Println("==================")
+	for _, num := range primerSlice{
+		fmt.Println(num)
+	}
+	fmt.Println("==================")
+	//  Para agregar variables de distinto tipo en un array o en un slice
+	//  Se debe aplicar una interfaz vacía... La interfaz "envuelve" a los datos
+	//  antes de ser almacenados en el array
+
+	segSlice := []interface{}{"Ferrnando", 27,true,4.09}
+	fmt.Println(segSlice)
+	fmt.Println("==================")
+
+	for _, num := range segSlice{
+		fmt.Println(num)
+	}
+}
