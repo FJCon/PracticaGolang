e7ed7b96e235aa724a8d8658d1c94aab1d96da0f
FJCon <conilfernando@gmail.com>
2023-04-21 20:50:37 -0300

    Estructuras

---
+++ 03 - Estructuras/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	//  Las estructuras se utilizan pararepresentar objetos en Go
+	//  Se inicializan con la palabra 'type' seguido del nombre y 
+	//  luego entre llaves los valores de la estructura
+}
