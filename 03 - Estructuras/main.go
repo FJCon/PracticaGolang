e80ba9c1996b6da97cf62efefd68ee80a5c7de28
FJCon <conilfernando@gmail.com>
2023-04-22 17:38:16 -0300

    Practica estructuras simples

--- 03 - Estructuras/main.go
+++ 03 - Estructuras/main.go
@@ -6,4 +6,19 @@ func main() {
 	//  Las estructuras se utilizan pararepresentar objetos en Go
 	//  Se inicializan con la palabra 'type' seguido del nombre y 
 	//  luego entre llaves los valores de la estructura
+
+	type Person struct{
+		nombre string
+		apellido string
+	}
+
+	francisco := Person{
+		nombre: "Francisco",
+		apellido: "Nessier",
+	}
+	
+	francisco.nombre = "Francisco Pedro" // Modifico un valor de un atributo
+
+	fmt.Println(francisco) // Verifico lo modificado
+
 }
