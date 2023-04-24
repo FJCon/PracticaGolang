6711b8b45e6c544b7aec2099631bf17336229f3f
FJCon <conilfernando@gmail.com>
2023-04-24 13:48:04 -0300

    estructuras fin

--- 03 - Estructuras/main.go
+++ 03 - Estructuras/main.go
@@ -16,11 +16,15 @@ type Person struct {
 //  Los método para un struct se declaran fuera de éste
 // con una funcion a la que se le debe indicar a que struct
 // aplicar el método, seguido del nombre del metodo y su lógica
+
 func (p *Person) Saludar(){
 	fmt.Println("Hola mi nombre es:", p.Nombre, p.Apellido)
 }
 
+
 func main() {
+
+	//
 	francisco := Person{
 		Nombre: "Francisco",
 		Apellido: "Nessier",
