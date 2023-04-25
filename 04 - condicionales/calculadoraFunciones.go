076fd245bda3b02de5df71ac6eee5c9d687e7375
FJCon <conilfernando@gmail.com>
2023-04-25 11:42:53 -0300

    calculadora a base de funciones

--- 04 - condicionales/calculadoraFunciones.go
+++ 04 - condicionales/calculadoraFunciones.go
@@ -1,9 +1,36 @@
 package main
 
 import "fmt"
-func suma(a,b float64) float64{
-	
-}
+
 func main(){
+	suma := func(a float64,b float64) float64{
+		resultado := a+b
+		return resultado
+	}
+
+	fmt.Println(suma(1,3))
+
+	resta := func(a float64,b float64)float64{
+		resultado := a - b
+		return resultado
+	}
+	
+	mult := func(a float64, b float64)float64{
+		resultado := a * b
+		return resultado
+	}
+	div := func(a float64, b float64) float64{
+		resultado := a / b
+		return resultado
+	}
+
+	fmt.Println("Resultado suma:")
+	fmt.Println(suma(3,5))
+	fmt.Println("Resultado resta:")
+	fmt.Println(resta(3,9))
+	fmt.Println("Resultado multiplicación:")
+	fmt.Println(mult(3,-1))
+	fmt.Println("Resultado división:")
+	fmt.Println(div(1,2))
 
 }
