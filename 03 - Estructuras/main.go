abbfeba9ef3843122dd3bfa0fedce5fd848f4fd4
unknown <conilfernando@gmail.com>
2024-08-15 11:36:52 -0300

    actualizacion caprta estructuras

--- 03 - Estructuras/main.go
+++ 03 - Estructuras/main.go
@@ -17,12 +17,26 @@ type Person struct {
 	Edad int
 }
 
+
 //  Los método para un struct se declaran fuera de éste
 // con una funcion a la que se le debe indicar a que struct pertenece
 // aplicar el método, seguido del nombre del metodo y su lógica
 
 func (p *Person) Saludar(){
 	fmt.Println("Hola mi nombre es:", p.Nombre, p.Apellido)
+}
+
+
+
+type Persona struct {
+	Nombre string
+	Edad int
+}
+
+// Se puede modificar lo que se muestra al utilizar fmt.Println si
+// creamos un metodo llamado String() que devuelva un textoo formateado
+func (p Persona) String() string{
+	return fmt.Sprintf("%v (%v años)",p.Nombre,p.Edad)
 }
 
 
@@ -35,15 +49,19 @@ func main() {
 		Edad: 28,
 	}
 
+	var fernando = Persona{
+		Nombre:"Fernando",
+		Edad: 27,
+	}
+
 
 	francisco.Nombre = "Francisco Pedro" // Modifico un valor de un atributo
 
 	fmt.Println(francisco) // Verifico lo modificado
 	francisco.Saludar()
 
-
-	//  Los  métodos en Go no se definen dentro del struct
-	//  Se declaran desde fuera con la siguiente sintaxis
+	fmt.Println("===========================================")
+	fmt.Println(fernando) //Al modificar elmetodo String() esto devuelve Fernando (27años) en vez de {Fernando, 27}
 
 }
 
